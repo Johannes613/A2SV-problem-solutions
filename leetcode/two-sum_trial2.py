454d182bd5084b4e1dd99662c27847d2e9fd462f
Yohannis Adamu <160431822+Johannes613@users.noreply.github.com>
2026-02-24 15:40:41 +0400

    Add solution for Two Sum (trial 2)

---
+++ leetcode/two-sum_trial2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in dictionary:
+                return [dictionary.get(diff),i]
+            dictionary[nums[i]] = i
